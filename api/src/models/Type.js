842ba500e7a483d03c0fb4377724fcfeecd5ed5a
ale9516 <juan.gutierrez19958@gmail.com>
2023-01-27 23:59:05 -0300

    models starting

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -3,17 +3,10 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    id:{
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-      defaultValue: DataTypes.UUIDV4
-    },
+  sequelize.define('type', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    
   });
 };
