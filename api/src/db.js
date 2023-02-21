1eb2334cb07ec9e7dc807df1d0edb36a8f3d5a89
ale9516 <juan.gutierrez19958@gmail.com>
2023-02-21 07:21:12 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,20 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_RAILWAY
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+/*const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});*/
+
+const sequelize = new Sequelize(DB_RAILWAY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
